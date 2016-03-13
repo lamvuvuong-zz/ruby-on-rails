73ebfbe983e4cb4d887dfdf7c2b4c743d17b9d38
Vuong Lam Vu <lamvuvuong@gmail.com>
2016-03-14 00:19:53 +0700

    Finish some function

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap', '~> 4.0.0.alpha3'
 
+gem 'kaminari', '~> 0.16.1'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,13 @@
 class MenuController < ApplicationController
   def index
+  	@sections = %w(Breakfast Lunch Dinner Drinks)
+  	@orders = ["A->Z", "Date Ascending", "Date Descending"]
+  	if (params[:order] == "Date Ascending")
+  		@food_items = FoodItem.filter_by_section(params[:section]).order(created_at: :asc).page(params[:page]).per(5)
+  	elsif (params[:order] == "Date Descending")
+  		@food_items = FoodItem.filter_by_section(params[:section]).order(created_at: :desc).page(params[:page]).per(5)
+  	else
+  		@food_items = FoodItem.filter_by_section(params[:section]).order('name ASC').page(params[:page]).per(5)
+  	end	
   end
 end
